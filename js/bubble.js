9c75c51bb6f08c18e397d1e1ea4fc6d98c9bd01b
Vladislav Telichko <vladislav.telichko@ya.ru>
2018-10-27 14:32:20 +0300

    Bubble sort

---
+++ js/bubble.js
@@ -0,0 +1,18 @@
+function bubbleSort(array) {
+    
+    for (let i = 0; i < array.length; i++) {
+       for (let j = 0; j < array.length - i; j++) {
+
+           if (j+1 < array.length && array[j] > array[j+1]) {
+               let tmp = array[j+1];
+
+               array[j+1] = array[j];
+               array[j] = tmp;
+
+           }
+           
+       } 
+    }
+
+    return array;
+}
